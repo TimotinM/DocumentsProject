fffb4fe324de8e85e91bfd57b54b1e3d332ffbfb
Timotin <mihaitimotin@gmail.com>
2023-08-30 11:56:59 +0300

    Add

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Domain.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Infrastructure.Data.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int IdInstitution { get; set; }
+        public bool IsEnabled { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+    }
+}
